84a3ecd73e363c13e016a674a2b7e7b0a47e39fe
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-11-07 17:20:14 +0530

    Remove scroll indicator with down arrow from Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -78,16 +78,6 @@ export default function Hero() {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-12 left-1/2 transform -translate-x-1/2 animate-bounce-slow z-10">
-        <div className="flex flex-col items-center space-y-2">
-          <span className="text-sm text-gray-600 dark:text-gray-400 font-medium">Scroll</span>
-          <svg className="w-6 h-6 text-gray-600 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-          </svg>
-        </div>
-      </div>
-
       {/* Quote Modal */}
       <QuoteModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </section>
